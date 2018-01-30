2a796d9855afbe8b502ba45ed14dbd7807c35c53
Matthew <MattKeenum@gmail.com>
2018-01-30 14:48:26 -0600

    C# Selenium WebDriver Tutorial

---
+++ WebDriverDemo/WebDriverDemo/Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.IE;
+using OpenQA.Selenium.Remote;
+using OpenQA.Selenium.Support.UI;
+
+namespace WebDriverDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //IWebDriver driver = new ChromeDriver(@"C:\Users\mattk\webdrivers\");
+            FirefoxOptions ffo = new FirefoxOptions();
+
+            // Using Selenium Standalone Server v3.8.1
+            Uri uri = new Uri("http://localhost:4444/wd/hub");
+            RemoteWebDriver driver = new RemoteWebDriver(uri, ffo.ToCapabilities());
+
+            driver.Url = "http://www.google.com";
+
+            var searchBox = driver.FindElement(By.Id("lst-ib"));
+            searchBox.SendKeys("Pluralsight");
+            searchBox.Submit();
+
+
+            //findByClassNameImplicitWait(driver);
+            findByClassNameExplicitWait(driver);
+            //findByXPath(driver);
+            //findByCssSelector(driver);
+
+            var img = driver.FindElement(By.ClassName("rg_i"));
+            img.Click();
+            
+
+        }
+
+        // Example of finding element by Class Name with an implicit wait
+        static void findByClassNameImplicitWait(IWebDriver driver)
+        {
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+
+            /*  This specific example is finding a compound class name ("q qs"), which Selenium doesn't support
+                To get around this, we are only searching on the last class ("qs") and then filtering the results */
+            var links = driver.FindElements(By.ClassName("qs"));
+            foreach (var link in links)
+            {
+                if (link.Text == "Images")
+                {
+                    link.Click();
+                    break;
+                }
+            }
+
+        }
+
+        // Example of finding element by Class Name with an explicit wait
+        static void findByClassNameExplicitWait(IWebDriver driver)
+        {
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(5));
+            var links = wait.Until(d =>
+            {
+                var elements = driver.FindElements(By.ClassName("qs"));
+                if (elements.Count > 0)
+                {
+                    return elements;
+                }
+                return null;
+            });
+
+            /*  This specific example is finding a compound class name ("q qs"), which Selenium doesn't support
+                To get around this, we are only searching on the last class ("qs") and then filtering the results */
+            foreach (var link in links)
+            {
+                if (link.Text == "Images")
+                {
+                    link.Click();
+                    break;
+                }
+            }
+
+        }
+
+        // Example of finding an element by XPath
+        static void findByXPath(IWebDriver driver)
+        {
+            var link = driver.FindElement(By.XPath("//*[@id=\"hdtb-msb-vis\"]/div[4]/a"));
+            link.Click();
+        }
+
+        // Example of finding an element by CssSelector
+        static void findByCssSelector(IWebDriver driver)
+        {
+            var links = driver.FindElements(By.CssSelector(".q.qs"));
+            foreach (var link in links)
+            {
+                if (link.Text == "Images")
+                {
+                    link.Click();
+                    break;
+                }
+            }
+        }
+    }
+}
---
+++ WebDriverDemo2/WebDriverDemo2/Program.cs
@@ -0,0 +1,69 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System;
+
+namespace WebDriverDemo2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver driver = new ChromeDriver(@"C:\Users\Mattk\webdrivers\");
+            driver.Url = @"file:///C:/Users/mattk/source/repos/WebDriverDemo2/WebDriverDemo2/TestPage.html";
+
+            //CheckRadioButtons(driver);
+            //CheckCheckBoxes(driver);
+            //CheckSelectLists(driver);
+            //FindNestedTablesWithTagName(driver);
+            //FindNestedTablesWithXPath(driver);
+
+
+
+
+        }
+
+        static void CheckRadioButtons(IWebDriver driver)
+        {
+            var radioButtons = driver.FindElements(By.Name("color"));
+            foreach (var radioButton in radioButtons)
+            {
+                if (radioButton.Selected)
+                {
+                    Console.WriteLine(radioButton.GetAttribute("value"));
+                }
+            }
+        }
+
+        static void CheckCheckBoxes(IWebDriver driver)
+        {
+            var checkBox = driver.FindElement(By.Id("check1"));
+            checkBox.Click();
+        }
+
+        static void CheckSelectLists(IWebDriver driver)
+        {
+            var select = driver.FindElement(By.Id("select1"));
+
+            var selectElement = new SelectElement(select);
+            selectElement.SelectByText("Frank");
+        }
+
+        static void FindNestedTablesWithTagName(IWebDriver driver)
+        {
+            var outerTable = driver.FindElement(By.TagName("table"));
+            var innerTable = outerTable.FindElement(By.TagName("table"));
+            var rows = innerTable.FindElements(By.TagName("td"));
+
+            foreach (var row in rows)
+            {
+                Console.WriteLine("TagName Row: " + row.Text);
+            }
+        }
+        static void FindNestedTablesWithXPath(IWebDriver driver)
+        {
+            var row = driver.FindElement(By.XPath("/ html / body / table / tbody / tr / td[2] / table / tbody / tr[2] / td"));
+            Console.WriteLine("XPath Row: " + row.Text);
+        }
+    }
+}
